4e6ca6eca616b35d5af6e9462085ed0727985c22
Tulus04 <arzitul@gmail.com>
2025-04-14 22:58:19 +0800

    commit kedua

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -360,43 +360,47 @@
 
   <footer id="footer" class="footer position-relative dark-background">
 
+    
+
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
           <a href="index.html" class="d-flex align-items-center">
-            <span class="sitename">Eterna</span>
+            <span class="sitename">Rekayasa dan Komnputer</span>
           </a>
           <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
+            <p>Samratulangi, Sungai Keledang,</p>
+            <p>Kec. Samarinda Seberang,</p>
+            <p>Kota Samarinda,</p>
+            <p>Kalimantan Timur 75131</p>
+            <p class="mt-3"><strong>No. Telepon:</strong> <span>(0541) 260421, 260680</span></p>
+            <p><strong>Email:</strong> <span>politanismd@gmail.com</span></p>
           </div>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
+          <h4>Tentang Jurusan</h4>
           <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="Visi-Misi.html">Visi&Misi</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="Struktur-Organisasi.html">Struktur Organisasi</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="akreditasi-prodi.html">Akreditasi Program Studi</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="hubungi_kami.html">Hubungi kami</a></li>
           </ul>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
+          <h4>Web Prodi</h4>
           <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://geomatika.politanisamarinda.ac.id/">Teknologi Geomatika</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Sistem Informasi Akuntansi</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Tekologi Rekayasa Perangkat Lunak</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="https://geomatika.politanisamarinda.ac.id/">Teknologi Rekayasa Geomatika dan Survei</a></li>
           </ul>
         </div>
 
         <div class="col-lg-4 col-md-12">
-          <h4>Follow Us</h4>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+          <h4>Follow Kami</h4>
+          <p>Follow Sosial Media Kami untuk Mendapatkan Update Informasi Terbaru Seputar Kampus</p>
           <div class="social-links d-flex">
             <a href=""><i class="bi bi-twitter-x"></i></a>
             <a href=""><i class="bi bi-facebook"></i></a>
@@ -409,13 +413,13 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Eterna</strong> <span>All Rights Reserved</span></p>
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Rekayasa&Komputer</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://bootstrapmade.com/">Kelompok 15</a>
       </div>
     </div>
 
